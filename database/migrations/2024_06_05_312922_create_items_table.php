cda5dd31dacb68cda874951bddbc36ae83544827
Valentino Janjac <valentino.janjac@bozic.io>
2024-12-28 13:37:27 +0100

    Fix trans

--- database/migrations/2024_06_05_312922_create_items_table.php
+++ database/migrations/2024_06_05_312922_create_items_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->timestamps();
             $table->unsignedBigInteger('price');
             $table->json('name');
-            $table->json('description')->nullable();
+            $table->json('description');
             $table->string('img')->nullable();
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
